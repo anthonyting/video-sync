8bbe041508dbf93adb6face81c7eae0c85fa6488
Anthony Ting <anthonydting@gmail.com>
2021-02-21 18:44:02 -0800

    fix bad initialization of redis session

--- session.js
+++ session.js
@@ -2,10 +2,10 @@ const session = require('express-session');
 
 const RedisStore = require('connect-redis')(session);
 const config = require('./config');
+const redis = require('./src/redis');
 let sessionStore= new RedisStore({
   client: redis
 });
-const redis = require('./src/redis');
 console.log("Setting up redis session store");
 const sessionParser = session({
   cookie: {
